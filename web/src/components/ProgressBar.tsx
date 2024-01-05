9110e06d233ab97d04ca9398af70dcc248518545
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-05 18:20:48 -0300

    feat: finish day detais popover

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface ProgresBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: ProgresBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progresBar"
+        aria-Label="Progresso de Hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600 w-3/4"
+        style={{
+          width: `${props.progress}%`,
+        }}
+      ></div>
+    </div>
+  );
+}
