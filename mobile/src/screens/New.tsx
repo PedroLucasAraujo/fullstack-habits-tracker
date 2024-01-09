7dcdaad4a97cc7552f11417bd7243c728b5983d2
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-09 13:11:14 -0300

    feat: creating back button, implementing back button on creation habit page, creating new input habits, creating checkbox component, listing a checkbox por every weekday, creating toogle checkbox function

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,5 +1,60 @@
-import { View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+import { useState } from "react";
+
+const availableWeekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 export function New() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Create Habit
+        </Text>
+
+        <Text className="mt-6 text-white font-semibold text-base">
+          What is your commitment?
+        </Text>
+
+        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          What is the recurrence?
+        </Text>
+
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox
+            title={weekDay}
+            key={weekDay}
+            checked={weekDays.includes(index)}
+            onPress={() => {
+              handleToggleWeekDay(index);
+            }}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
 }
