56b88fc37df9b3b32cff7ace49f341700a8d46b2
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-01-15 13:30:39 -0300

    feat: adding transitions and focus

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -30,19 +30,22 @@ export function HabitDay({
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10 rounded-lg", {
-          " bg-zinc-900 border-2 border-zinc-800": completedPercentage === 0,
-          "bg-violet-900 border-violet-700":
-            completedPercentage > 0 && completedPercentage < 20,
-          "bg-violet-800 border-violet-600":
-            completedPercentage >= 0 && completedPercentage < 20,
-          "bg-violet-700 border-violet-500":
-            completedPercentage >= 0 && completedPercentage < 40,
-          "bg-violet-600 border-violet-500":
-            completedPercentage >= 0 && completedPercentage < 60,
-          "bg-violet-500 border-violet-400":
-            completedPercentage >= 0 && completedPercentage < 80,
-        })}
+        className={clsx(
+          "w-10 h-10 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-background",
+          {
+            " bg-zinc-900 border-2 border-zinc-800": completedPercentage === 0,
+            "bg-violet-900 border-violet-700":
+              completedPercentage > 0 && completedPercentage < 20,
+            "bg-violet-800 border-violet-600":
+              completedPercentage >= 0 && completedPercentage < 20,
+            "bg-violet-700 border-violet-500":
+              completedPercentage >= 0 && completedPercentage < 40,
+            "bg-violet-600 border-violet-500":
+              completedPercentage >= 0 && completedPercentage < 60,
+            "bg-violet-500 border-violet-400":
+              completedPercentage >= 0 && completedPercentage < 80,
+          }
+        )}
       ></Popover.Trigger>
 
       <Popover.Portal>
--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -63,11 +63,11 @@ export function HabitsList({ date }: HabitsListProps) {
           <Checkbox.Root
             key={habit.id}
             checked={habitsInfo.completedHabits.includes(habit.id)}
-            className="flex items-center gap-3 px-0 group outline-none"
+            className="flex items-center gap-3 px-0 group outline-none focus:outline-none disabled:cursor-not-allowed"
             disable={isDateInPast}
             onCheckedChange={() => handleToggleHabit(habit.id)}
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
